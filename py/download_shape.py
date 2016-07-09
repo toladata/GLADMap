d3723d76b6d416ff5d1ea55dea0a7d0f7eb67d24
Madhav Sharan <msharan@paypal.com>
2016-07-09 15:47:34 -0700

    Code to extract shp file from zip

--- py/download_shape.py
+++ py/download_shape.py
@@ -3,6 +3,7 @@ Downlaods shape file for all intrested countries and place it in tmp folder of p
 Example path - http://biogeo.ucdavis.edu/data/gadm2.8/shp/AFG_adm_shp.zip
 '''
 import urllib
+import zipfile
 
 template_str = "http://biogeo.ucdavis.edu/data/gadm2.8/shp/{0}_adm_shp.zip"
 file_dir = "../tmp/"
@@ -12,6 +13,20 @@ list_country = ['AFG','CHN']
 for country in list_country:
 	path = template_str.format(country)
 	print "Trying path - " + path
-	file_name = file_dir + path.split("/")[-1]
-	urllib.urlretrieve (path, file_name)
-	print "Saved in - " + file_name
+	file_name = path.split("/")[-1]
+	file_path = file_dir + file_name
+	urllib.urlretrieve (path, file_path)
+	print "Saved in - " + file_path
+	zip_ref = zipfile.ZipFile(file_path, 'r')
+	try:
+		zip_ref.extract(country+"_adm3.shp",file_dir)
+		print "Saved " + country + "_adm3.shp"
+	except Exception, e:
+		try:
+			zip_ref.extract(country+"_adm2.shp",file_dir)
+			print "Saved " + country + "_adm2.shp"
+		except Exception, e:
+			print "Cant find shape file for " + file_name
+
+	
+	zip_ref.close()
