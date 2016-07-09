80e43829498c8617b4b7da39464ce1a530e13b84
sapratap <sapratap>
2016-07-09 15:44:40 -0700

    Map change styles

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-  
+  var files,layer;
   var geojsonFeature = {
 		    "type": "Feature",
 		    "properties": {
@@ -14,22 +14,38 @@
 
 
   // initialize the map
-  var map = L.map('mapid').setView([36.99404, 39.75621], 3);
-
-
+  var map = L.map('mapid').setView([36.99404, 39.75621], 2);
   // load a tile layer
   L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
     {
-      attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
       maxZoom: 18,
-      minZoom: 2
+      minZoom: 1,
+      continuousWorld: false,
+      // this option disables loading tiles outside of the world bounds.
+      noWrap: true
     }).addTo(map);
+ 
   
   
-  d3.json("/data/AFG/afg_2.json", function(error, data) {
-	  
-	  L.geoJson(data).addTo(map);
-	});
   
+  d3.json("/data/file_locations.json", function(error, data) {
+	 files=data.files;
+	 addRegionalDetails(2);
+  });
+  
+  function addRegionalDetails(zoomLevel){
+	  if(layer)
+		  layer.clearLayers();
+	  var fileNo=parseInt(zoomLevel/6)+1;
+	  for (var i=0;i<files.length;i++){
+		  d3.json(files[i]+"_"+fileNo+".json",function(error, data) {
+			  layer=L.geoJson(data).addTo(map);
+			  layer.setStyle({weight :2}) 
+			});
+		  
+	  }
+  }
+  
+  map.on("zoomend", function (e) {addRegionalDetails(e.target['_animateToZoom']) });
  
 
