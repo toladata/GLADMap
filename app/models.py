104dda924ee24189acaa8bc216395ea12f627b4c
sapratap <sapratap>
2016-07-10 02:54:47 -0700

    changes

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from flask import Blueprint,  render_template,request
 from flask_cache import Cache
-
+import os
 from app import app
 from app import mongo
+import json
 
 mod_data = Blueprint('data', __name__)
 cache = Cache(app,config={'CACHE_TYPE': 'simple'})
@@ -13,4 +14,31 @@ def visualize():
     online_users = mongo.db.users.find({'online': True})
     print online_users
     return render_template('leaftlet.html')
+
+
+@mod_data.route("/get-uploaded-data")
+def get_uploaded_data():
+    allFiles=[]
+    for files in os.listdir("app/static/upload"):
+        allFiles.append(files)
+    return json.dumps({"files": allFiles})
+
+
+@mod_data.route("/upload-file", methods=['POST'])
+def upload_file():
+    print request
+    files = request.files.getlist('file')[0]
+    filename= request.values['fileNames']
+    print filename
+    if files:
+        directory_path =  os.path.join('app/static/upload')
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path, mode=0777)
+            os.chmod(directory_path, 0777)
+        if files:
+            files.save(directory_path+'/'+filename)
+        else:
+            return json.dumps({"success": False})
+    return json.dumps({"success": True})
+
 
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,7 +1,20 @@
-  var files,layer;
+$(function(){
+
+    $.ajax({
+       url: "/get-uploaded-data",
+       type: "GET"
+     }).done(function( data ) {
+        var files=JSON.parse(data).files;
+        for(var i in files){
+             $('#uploadedList').append($("<option>").attr('value',files[i]).text(files[i]));
+        }
+     });
+
+  var files,layers=[],features,featuresInformation;
 
   // initialize the map
   var map = L.map('mapid').setView([36.99404, 39.75621], 2);
+  var markers = new L.FeatureGroup();
   // load a tile layer
      L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
 //    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -17,24 +30,63 @@
   d3.json("/static/data/file_locations.json", function(error, data) {
 	 files=data;
 	 for (var i in files)
-      $('#countryList').append($("<option>").attr('value',files[i].file_name).text(files[i].country_name));
+        $('#countryList').append($("<option>").attr('value',files[i].file_name).text(files[i].country_name));
+     $("#countryList").multiselect();
   });
 
 
 $("#countryList").on('change',function(){
-      if(layer)
-          layer.clearLayers();
+      if(layers){
+        for(var i=0;i<layers.length;i++){
+            layers[i].clearLayers();
+        }
+      }
+      layers=[];
+
       var zoomLevel=2;
-      d3.json('/static/data/'+$(this).val(),function(error, data) {
-          layer=L.geoJson(data, {
-                onEachFeature: onEachFeature
-            }).addTo(map)
+      var countries=$(this).val();
+      if(!countries)
+        return;
+      for (var val=0;val<countries.length;val++){
+          d3.json('/static/data/'+countries[val],function(error, data,val) {
+                layers.push(L.geoJson(data, {
+                      onEachFeature: onEachFeature
+                  }).addTo(map));
 
-          layer.setStyle({weight :2,opacity:(zoomLevel)/18});
+                layers[layers.length-1].setStyle({weight:2,opacity:(zoomLevel)/18});
 
-        });
-})
+              });
+      }
 
+});
+
+$("#uploadedList").on('change',function(){
+      $("#getNearest").hide();
+
+      if(features){
+       features.clearLayers();
+      }
+       $(".loader").show();
+      d3.json('/static/upload/'+$(this).val(),function(error, data,val) {
+        	  featuresInformation = L.geoJson(data, {
+        		    onEachFeature: onEachFeature
+        		});
+        		try{
+        		    var markers = L.markerClusterGroup();
+                      features=markers.addLayer(featuresInformation);
+                      map.addLayer(markers);
+        		}
+        		catch(err){
+        		    features = featuresInformation.addTo(map);
+        		}
+
+        		 $(".loader").hide();
+
+        		$("#getNearest").show();
+
+
+          });
+});
 
 
 
@@ -63,20 +115,51 @@ $("#countryList").on('change',function(){
 
   map.on("zoomend", function (e) {
 	  var zoomLevel=e.target['_animateToZoom'];
-//	  for (var i=0;i<layers.length;i++){
-//		  layers[i].setStyle({opacity: (zoomLevel)/18});
-//		  }
+	  for (var i=0;i<layers.length;i++){
+		  layers[i].setStyle({opacity: (zoomLevel)/18});
+		  }
   });
- 
 
 
-//  d3.json("/data/afghanistan-data/afghanistan.geojson", function(error, data) {
-////	  L.geoJson(data).addTo(map);
-//	  var information = L.geoJson(data, {
-//		    onEachFeature: onEachFeature
-//		});
-//	  var markers = L.markerClusterGroup();
-//	  markers.addLayer(information);
-//	  map.addLayer(markers);
-//  });
+ $("#showUploader").on('click',function(){
+    $(".file-upload-holder").show();
 
+ })
+
+ $("#uploadFile").on("click",function(){
+     console.log("submit event");
+     var fd = new FormData($('form')[0]);
+     $.ajax({
+       url: "/upload-file",
+       type: "POST",
+       data: fd,
+       processData: false,  // tell jQuery not to process the data
+       contentType: false   // tell jQuery not to set contentType
+     }).done(function( data ) {
+         location.reload();
+     });
+     return false;
+ })
+
+$("#getNearest").on('click',function(){
+    markers.clearLayers();
+    $(".loader").show();
+    $.getJSON('http://ipinfo.io', function(data){
+         var lat=data.loc.split(",")[0];
+         var lon=data.loc.split(",")[1];
+
+         var nearest = leafletKnn(featuresInformation).nearest(L.latLng(lat, lon), 3);
+         for (var i=0;i<nearest.length;i++){
+           var marker=L.marker([nearest[i].lat, nearest[i].lon]).bindPopup("Here");
+            markers.addLayer(marker);
+         }
+         console.log(nearest)
+         map.addLayer(markers);
+
+         $(".loader").hide();
+})
+
+
+})
+
+});
