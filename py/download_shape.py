3a83ddeae495bdc2cde6a90d8a7c610fe2597673
Madhav Sharan <msharan@paypal.com>
2016-07-09 18:58:21 -0700

    geojson for all countries

--- py/download_shape.py
+++ py/download_shape.py
@@ -8,31 +8,45 @@ import zipfile
 template_str = "http://biogeo.ucdavis.edu/data/gadm2.8/shp/{0}_adm_shp.zip"
 shp_file_dir = "../tmp/"
 
-list_country = ['AFG','CHN']
+list_country = ['NER','NGA','PAK','SOM','SSD','LKA','SDN','SYR','TJK','TLS','TUN','TUR','UGA','UKR','USA','XWB','XGZ','YEM','ZWE',
+'CAF', 'CHN', 'COL', 'COD', 'EGY', 'ETH', 'GEO', 'GTM', 'HTI', 'HND', 'IND', 'IDN', 'IRQ', 'JPN', 'JOR', 'KEN', 'XKX', 'KGZ', 'LBN', 'LBR', 'LBY', 'MLI', 'MNG', 'MAR', 'MMR', 'NPL']
 
 def download_country(country):
-	path = template_str.format(country)
-	print "Trying path - " + path
-	file_name = path.split("/")[-1]
+	shape_file_path = ""
+	download_url = template_str.format(country)
+	print "Trying path - " + download_url
+	file_name = download_url.split("/")[-1]
 	file_path = shp_file_dir + file_name
-	urllib.urlretrieve (path, file_path)
+	urllib.urlretrieve (download_url, file_path)
 	print "Saved in - " + file_path
 	zip_ref = zipfile.ZipFile(file_path, 'r')
+
 	try:
 		zip_ref.extract(country+"_adm3.shp",shp_file_dir)
 		zip_ref.extract(country+"_adm3.shx",shp_file_dir)
-		print "Saved " + country + "_adm3.shp"
+		shape_file_path = shp_file_dir + country + "_adm3.shp"
+		print "Saved " + shape_file_path
 	except Exception, e:
 		try:
 			zip_ref.extract(country+"_adm2.shp",shp_file_dir)
 			zip_ref.extract(country+"_adm2.shx",shp_file_dir)
-			print "Saved " + country + "_adm2.shp"
+			shape_file_path = shp_file_dir + country + "_adm2.shp"
+			print "Saved " + shape_file_path
 		except Exception, e:
 			print "Cant find shape file for " + file_name
 
 	zip_ref.close()
+	return shape_file_path
 
 if __name__ == "__main__":
 	for country in list_country:
-		download_country(country)
+		try:
+			download_country(country)
+		except Exception, e:
+			print e
+			print ":::::"
+			print "Can't process - " + country
+			print ":::::"
+
+		
 
